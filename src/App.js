3d696432ea7a7af58cde68110907760c8748a13c
6ixty9 <72061948+6ixty9@users.noreply.github.com>
2022-09-23 16:29:37 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Offer from "./components/Offer";
 import BestSellers from "./components/BestSellers";
 import BrowseCategory from "./components/BrowseCategory";
 import Authors from "./components/Authors";
+import Newsletter from "./components/Newsletter";
 import Footer from "./components/Footer";
 import JsonData from "./data/data.json";
 import { useEffect, useState } from "react";
-import Newsletter from "./components/Newsletter";
-import BrowseCategory from "./components/BrowseCategory";
 
 
 function App() {
@@ -22,7 +21,7 @@ function App() {
       <Hero data={landingPageData.Hero} />
       <Offer data={landingPageData.Offer} />
       <BestSellers data={landingPageData.BestSellers} />
-      <BrowseCategory></BrowseCategory>
+      <BrowseCategory/>
       <Authors data={landingPageData.Authors} />
       <Newsletter/>
       <Footer />
