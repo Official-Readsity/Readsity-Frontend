414359ed1e49e6e9577812f7eec706ef1f8f5296
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-09-24 19:04:13 +0100

    I modify sections of the page to be centralize

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,30 +4,30 @@ import logo from "../assets/readsitynav.png";
 
 const Navbar = (props) => {
   return (
-    <div className='header'>
+		<div className="navbar_section">
+			<div className="navbar_container">
+				<div className="header">
+					<div className="logodiv">
+						<img className="logo" src={logo} alt="main_logo" />
+					</div>
 
-        <div className="logodiv">
-            <img className="logo" src={logo} alt="main_logo"/>
-        </div>
+					<div className="navi">
+						<ul className="navi">
+							<li className="nav_items">Home</li>
+							<li className="nav_items">Services</li>
+							<li className="nav_items">Blogs</li>
+							<li className="nav_items">Books</li>
+						</ul>
+					</div>
 
-        <div className="navi">
-
-                <ul className="navi">
-                  <li>Home</li>
-                  <li>Services</li>
-                  <li>Blogs</li>
-                  <li>Books</li>
-                </ul>
-              </div>
-
-              <div className="cta">
-                <button className="signUp">Get Started</button>
-                <button className="signIn">Sign In</button>
-              </div>
-        
-
-    </div>
-  )
+					<div className="cta">
+						<button className="signUp action_button">Get Started</button>
+						<button className="signIn action_button">Sign In</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar
