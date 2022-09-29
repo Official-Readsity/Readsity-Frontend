bed73ad3aa6e460ccbdb0490cf631bf6be8ae308
emma-junior <emnzboy31@gmail.com>
2022-09-29 01:26:47 +0100

    bestseller-responsiveness

--- src/components/BrowseCategory.js
+++ src/components/BrowseCategory.js
@@ -6,18 +6,20 @@ import "./browsecategory.css";
 
 const BrowseCategory = () => {
   return (
-		<section className="browsecategory">
-				<div className="explore">
-					<h2>Explore and read thousand of books online</h2>
-					<button>Browse our Categories</button>
-				</div>
-				<div className="pictures">
-					<img className="country-img" src={country} alt="" />
-					<img className="george-img" src={georgeOrwell} alt="" />
-					<img className="simplicity-img" src={simplicity} alt="" />
-				</div>
-		</section>
-	);
+    <section className="browsecategory">
+      <div className="browsecategory-wrapper">
+        <div className="explore">
+          <h2>Explore and read thousand of books online</h2>
+          <button>Browse our Categories</button>
+        </div>
+        <div className="pictures">
+          <img className="country-img" src={country} alt="" />
+          <img className="george-img" src={georgeOrwell} alt="" />
+          <img className="simplicity-img" src={simplicity} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BrowseCategory;
