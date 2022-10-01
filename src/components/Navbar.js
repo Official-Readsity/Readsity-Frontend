62e198088e22336bd9dc6dec3bdc3675aa71c1a5
6ixty9 <72061948+6ixty9@users.noreply.github.com>
2022-10-01 14:20:55 +0100

    added mobile view for nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./NavStyles.css";
 import logo from "../assets/readsitynav.png";
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = (props) => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  
   return (
 		<div className="navbar_section">
 			<div className="navbar_container">
@@ -11,19 +15,28 @@ const Navbar = (props) => {
 						<img className="logo" src={logo} alt="main_logo" />
 					</div>
 
-					<div className="navi">
-						<ul className="navi">
+					<div className={click ? "navi active":"navi"}>
+						<ul className="navig">
 							<li className="nav_items">Home</li>
 							<li className="nav_items">Services</li>
 							<li className="nav_items">Blogs</li>
 							<li className="nav_items">Books</li>
-						</ul>
-					</div>
+							<li className="nav_items">Contact us</li>
 
 					<div className="cta">
 						<button className="signUp action_button">Get Started</button>
 						<button className="signIn action_button">Sign In</button>
 					</div>
+						</ul>
+          
+					</div>
+
+
+          <div className="hamburger" onClick={handleClick}>
+                {!click ?
+                (<FaBars className="Ham" size={20}/> ): 
+                (<FaTimes className="xIcon" size={20}/>) }
+            </div>
 				</div>
 			</div>
 		</div>
