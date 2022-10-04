408c96a12aa9d75c2fb76689acb525dcb45f1aad
emma-junior <emnzboy31@gmail.com>
2022-10-04 23:11:19 +0100

    bestseller-updated-ui

--- src/components/BestsellersCard.js
+++ src/components/BestsellersCard.js
@@ -5,10 +5,13 @@ const BestsellersCard = ({ bestseller }) => {
   console.log(bestseller);
   return (
     <div className="card">
-      <img src={bestseller.img} alt="" />
-      <div>
+      <div className={`${bestseller.background}`}>
+        <img className={`${bestseller.desc}`} src={bestseller.img} alt="" />
+      </div>
+      <div className="card-content">
         <h2 className="card-title">{bestseller.title}</h2>
         <p className="card-author">{bestseller.author}</p>
+        <button className="card-btn">Buy Book</button>
       </div>
     </div>
   );
