4f0e9d9f785db33f3e1a57e8507bce242e754c17
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-10-08 14:40:00 +0100

    I work on mobile responsiveness for section two of Home page

---
+++ src/components/romanceBooks.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./categoriesBook.css";
+import { Link } from "react-router-dom";
+import Books from '../data/bookCard.json'
+
+
+
+const romanceBooks = () => {
+  return (
+    <div>
+        {/* <div></div>     This is for the side navbar */}
+        <div className='right-container'>
+            <div className='top-container'>
+                <div>
+                    <h1>ROMANCE</h1>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                          <Link>RATING</Link>  
+                        </li>
+                        <li>
+                            <Link>NEWEST</Link> 
+                        </li>
+                        <li>
+                            <Link>AUTHOR</Link> 
+                        </li>
+                        <li>
+                            <Link>TITLE</Link> 
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className='bottom-container'>
+                {
+                    Books.map(card => {
+                        return(
+                            <div key={card.id}>
+                                {card.Romance.map(data => (
+                                    <div key={card.id}>
+                                        <div>
+                                            <img src={data.bookImage} alt="different type of books" />
+                                        </div>                                    
+                                        <div>
+                                            <p>{ data.bookTitle }</p>
+                                            <p>{ data.bookAuthor }</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default romanceBooks
